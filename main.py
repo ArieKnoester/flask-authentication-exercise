d0d4522b4a943524ab0acd35c5cdd9c9bbb4cb4e
Arie Knoester <arikstr@gmail.com>
2024-02-15 14:14:53 -0600

    Added secret key comment.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# How to generate good secret keys:
+# https://flask.palletsprojects.com/en/latest/quickstart/#sessions
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
